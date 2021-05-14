2ad096cba5245d9b1b522231b04942d6657f511b
austnaa <60988742+austnaa@users.noreply.github.com>
2021-05-13 19:51:35 -0700

    added observable/observer example demo

---
+++ observerPrototype/ObserverDemo.java
@@ -0,0 +1,18 @@
+package observerPrototype;
+
+import java.util.Observable;
+import java.util.Observer;
+
+@SuppressWarnings("deprecation")
+public class ObserverDemo implements Observer {
+    
+    // runs when the passed observable object notifies that a change was made
+    @Override
+    public void update(Observable observable, Object thePassedObj) {
+        System.out.println("int has changed to " + ((ObservableDemo) observable).getInt());
+        
+        
+       
+    }
+    
+}
