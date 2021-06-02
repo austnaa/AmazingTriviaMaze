82347a64a3027566390a52ea2253c6d601f74958
Daniel Jiang <jiang.s.daniel@gmail.com>
2021-06-01 19:17:52 -0700

    merged chau and daniel

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -37,7 +37,7 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
     private static final long serialVersionUID = 86445190678492115L;
     
     /** The delay between each game tick. */
-    private static final int TICK_DELAY = 30;
+    private static final int TICK_DELAY = 50;
     
     /**
      * The internal game timer.
