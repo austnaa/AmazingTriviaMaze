47e6c186707139be47986c2713c9d0c8a7effb6a
austnaa <60988742+austnaa@users.noreply.github.com>
2021-06-03 23:03:37 -0700

    added tests package

---
+++ src/tests/DoorTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DoorTest {
+
+    @Before
+    public void setUp() {
+        System.out.println("print");
+    }
+    
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
