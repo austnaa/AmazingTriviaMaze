2e9d52a373ca4a4fcfb64bdd477026ea9bfd1b3e
austnaa <60988742+austnaa@users.noreply.github.com>
2021-06-05 15:49:00 -0700

    started the TrueFalseQuestion test class

---
+++ src/tests/TrueFalseQuestionTest.java
@@ -0,0 +1,55 @@
+/**
+ * Amazing Trivia Maze
+ * TCSS 360 Spring 2021
+ */
+
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * A JUnit test class for the TrueFalseQuestion class.
+ * 
+ * @author Austn Attaway
+ * @version Spring 2021
+ */
+public class TrueFalseQuestionTest {
+
+    /**
+     * Tests the TrueFalseQuestion constructor for a NullPointerException
+     * when passed a null question String. 
+     */
+    
+    /**
+     * Tests the TrueFalseQuestion constructor for a NullPointerException
+     * when passed a null answer Option. 
+     */
+    
+    /**
+     * Tests the TrueFalseQuestion constructor for a NullPointerException
+     * when passed a null incorrect answer option.
+     */
+    
+    /**
+     * Tests the TrueFalseQuestion constructor for an IllegalArgumentException
+     * if it is passed an answer Option that is not as the correct option.
+     */
+    
+    /**
+     * Tests the TrueFalseQuestion constructor for an IllegalArgumentException
+     * if it is passed an incorrect answer Option that is not as the correct option.
+     */
+    
+    /**
+     * Tests the TrueFalseQuestion constructor to make sure the 
+     * given question String is set correctly.
+     */
+    
+    /**
+     * Tests the TrueFalseQuestion constructor to make sure the Options 
+     * are set to the same ButtonGroup.
+     */
+
+}
